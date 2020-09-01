474856e9e7c8a0af2b373d5064461b4f08c08918
Kohei Tsuchiya <doyahirohira@gmail.com>
2020-09-02 02:12:21 +0900

    インデント調整

--- internal/db/db.go
+++ internal/db/db.go
@@ -7,7 +7,7 @@ import (
 
 func Connection() *gorm.DB {
 	dsn := "smart-sr:smart-sr@tcp(127.0.0.1:3306)/smart-sr?charset=utf8mb4&parseTime=True&loc=Local"
-  db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
