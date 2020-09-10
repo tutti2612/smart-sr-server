4211a82d55898a14df6952969f8b9a83a44bd3b5
Kohei Tsuchiya <doyahirohira@gmail.com>
2020-09-10 23:27:14 +0900

    migration/main.goにも.envを読み込む処理を追加

--- cmd/migration/main.go
+++ cmd/migration/main.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
 	"github.com/tutti2612/smart-sr-server/internal/database"
 	"github.com/tutti2612/smart-sr-server/internal/models"
+	"log"
+	"os"
 )
 
 func main() {
+	envFile := ".env"
+	if env := os.Getenv("GO_ENV"); env != "" {
+		envFile += "." + env
+	}
+
+	if err := godotenv.Load(envFile); err != nil {
+		log.Fatal(err)
+	}
+
 	db := database.Connection()
-	db.AutoMigrate(&models.Student{})
+	if err := db.AutoMigrate(&models.Student{}); err != nil {
+		log.Fatal(err)
+	}
 }
